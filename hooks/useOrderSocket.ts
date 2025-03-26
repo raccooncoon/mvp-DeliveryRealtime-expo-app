79c7a4c37d3f420f4028671a3e9044888bd63188
임민영 <raccoon@shinsegae.com>
2025-03-26 15:33:55 +0900

    소켓 연결 샘플 앱

---
+++ hooks/useOrderSocket.ts
@@ -0,0 +1,43 @@
+import { useEffect, useRef, useState } from 'react';
+
+export function useOrderSocket(orderId: string) {
+  const socketRef = useRef<WebSocket | null>(null);
+  const [orderStatus, setOrderStatus] = useState('-');
+  const [customerId, setCustomerId] = useState('-');
+  const [log, setLog] = useState<string[]>([]);
+
+  useEffect(() => {
+    if (!orderId) return;
+
+    // let uri = `ws://localhost:8080`;
+    let uri = `ws://10.36.190.82:8080`;
+
+    const ws = new WebSocket(`${uri}/ws/orders`);
+    socketRef.current = ws;
+
+    ws.onopen = () => {
+      addLog(`🟢 WebSocket 연결됨: ${orderId}`);
+      ws.send(orderId);
+    };
+
+    ws.onmessage = (event) => {
+      const data = JSON.parse(event.data);
+      setOrderStatus(data.status);
+      setCustomerId(data.customerId);
+      addLog(`📦 상태 수신: ${event.data}`);
+    };
+
+    ws.onerror = () => addLog(`❌ WebSocket 오류 발생`);
+    ws.onclose = () => addLog(`🔌 연결 종료됨`);
+
+    return () => {
+      ws.close();
+    };
+  }, [orderId]);
+
+  const addLog = (msg: string) => {
+    setLog((prev) => [...prev, msg]);
+  };
+
+  return { orderStatus, customerId, log };
+}
